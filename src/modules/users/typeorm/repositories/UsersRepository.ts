6a3fb893f0b203f31a7a20d72d0667c2a518d242
joaowinderfeldbussolotto <joaowinderfeldbussolotto@gmail.com>
2023-12-05 18:06:25 -0300

    :sparkles: feat: users: entity, repo and services

---
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,22 @@
+import { EntityRepository, Repository } from 'typeorm';
+import User from '../entities/User';
+
+@EntityRepository(User)
+class UsersRepository extends Repository<User> {
+  public async findByName(name: string): Promise<User | undefined> {
+    const user = await this.findOne({ name });
+    return user;
+  }
+
+  public async findById(id: string): Promise<User | undefined> {
+    const user = await this.findOne({ id });
+    return user;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.findOne({ email });
+    return user;
+  }
+}
+
+export default UsersRepository;

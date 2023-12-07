5bb75a1670843319c0ada18708500199daed8188
joaowinderfeldbussolotto <joaowinderfeldbussolotto@gmail.com>
2023-12-07 10:49:31 -0300

    :sparkles: feat: sending fake email with ethreal and handlebars

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,8 @@ import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+import EtherealMail from '@config/mail/EtherealMail';
+import path from 'path';
 
 interface IRequest {
   email: string;
@@ -18,9 +20,26 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exist.');
     }
 
-    const token = await userTokensRepository.generate(user.id);
+    const { token } = await userTokensRepository.generate(user.id);
 
-    console.log(token);
+    await EtherealMail.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[API Vendas] Recuperação de Senha',
+      templateData: {
+        file: this.getForgotPasswordEmailTemplate(),
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
+  }
+
+  public getForgotPasswordEmailTemplate(): string {
+    return path.resolve(__dirname, '..', 'views', 'forgot_password.hbs');
   }
 }
 

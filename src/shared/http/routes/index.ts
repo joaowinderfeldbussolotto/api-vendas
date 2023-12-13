ac2c0277e2b1f71542ae25c2e579e3a6d56e8b6a
joaowinderfeldbussolotto <joaowinderfeldbussolotto@gmail.com>
2023-12-13 14:16:38 -0300

    :sparkles: feat: order controller and routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import customersRouter from '@modules/customers/routes/customer.routes';
+import ordersRouter from '@modules/orders/routes/orders.routes';
 import productsRouter from '@modules/products/routes/products.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
@@ -14,6 +15,7 @@ routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
 routes.use('/customers', customersRouter);
+routes.use('/orders', ordersRouter);
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Hello World!' });
